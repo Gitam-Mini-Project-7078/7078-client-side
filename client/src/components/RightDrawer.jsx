8295b4eba06b82dc09f50bc8d4094d00805ef191
Srihari Vemana <129419842+srihari-vemana@users.noreply.github.com>
2023-10-26 01:01:47 +0530

    Add RightDrawer

--- client/src/components/RightDrawer.jsx
+++ client/src/components/RightDrawer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Box, Button, Drawer, Grid, Input, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, TextField, Typography } from '@mui/material';
-import { CloudUpload, Inbox, Mail } from '@mui/icons-material';
+import { Box, Button, Divider, Drawer, Grid, Input, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, TextField, Typography, styled } from '@mui/material';
+import { CloudUpload, Inbox, Mail, Padding } from '@mui/icons-material';
 import { visuallyHidden } from '@mui/utils'
 
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
 const RightDrawer = (props) => {
+
   return (
     <Drawer
       anchor="right"
@@ -11,7 +25,7 @@ const RightDrawer = (props) => {
       onClose={props.onClose}
       PaperProps={{
         sx: {
-          width: '600px', // Adjust the width as needed
+          width: '600px',
         },
       }}
     >
@@ -19,9 +33,7 @@ const RightDrawer = (props) => {
         <ListItem>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <b>
               <ListItemText primary="Solution Title" />
-              </b>
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -46,18 +58,18 @@ const RightDrawer = (props) => {
             </Grid>
           </Grid>
         </ListItem>
-        <ListItem>
-          <Button variant="contained" color="primary" onClick={props.onClose}>
-            Save
-          </Button>
-        </ListItem>
-        <ListItem>
-            <Button component="label" variant="contained" startIcon={<CloudUpload />} sx={{maxWidth:'300px'}} size='small' >
-                Submit Solution
-                <Input sx={{ visuallyHidden }} type="file" />
-            </Button>
-        </ListItem>
       </List>
+      <Button component="label" variant="contained" startIcon={<CloudUpload />} sx={{minWidth:'400px', alignSelf:'center'}}>
+        Upload file
+        <VisuallyHiddenInput type="file" />
+      </Button>
+
+      <Divider sx={{paddingTop:1, paddingBottom: 2 }}/>
+
+      <Button variant="contained" color="primary" onClick={props.onClose} sx={{maxWidth:'200px', alignSelf:'center'}}>
+            Save
+      </Button>
+
     </Drawer>
   );
 };
